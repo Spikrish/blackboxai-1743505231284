7497c50661f2a596feed31908ab49ced6e7405cc
Spikrish <105040972+Spikrish@users.noreply.github.com>
2025-04-01 16:31:14 +0530

    Add mern-mobile-tracker/client/src/services/api.js 1743505273473

---
+++ mern-mobile-tracker/client/src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Add response interceptor
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
